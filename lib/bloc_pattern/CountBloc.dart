b54b0c649f4ecd3e7f751c2166bb55f8a68f5c72
Yoon Joo Lee <yoonjoolee@INV00043.fritz.box>
2022-02-15 11:54:46 +0100

    add bloc class

---
+++ lib/bloc_pattern/CountBloc.dart
@@ -0,0 +1,15 @@
+
+
+class CountBloc {
+  CountEventBloc countEventBloc = CountEventBloc();
+
+
+
+}
+
+class CountEventBloc{
+
+
+}
+
+enum CountEvent { ADD_EVENT, SUBTRTACT_EVENT}
--- lib/bloc_pattern/bloc_widget.dart
+++ lib/bloc_pattern/bloc_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BlocWidget extends StatefulWidget {
   @override
@@ -8,12 +9,84 @@ class BlocWidget extends StatefulWidget {
 
 class _BlockWidgetState extends State<BlocWidget> {
 
+  CountBloc countBloc;
+
+
 
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Bloc Pattern"),
+      ),
+      body: CountView(),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconButton(
+              onPressed:(){
+                //call bloc logic here instead of method
+                //register bloc event here
+              },
+              icon: Icon(Icons.add)),
+
+          IconButton(
+              onPressed:(){
+                //call bloc logic here instead of method
+                //register bloc event here
+              },
+              icon: Icon(Icons.remove)),
+
+        ],
+      ),,
+
+    );
   }
+
+  //Control lifecycle of Widget
+  // With Provider this part gets easier.
+  //When the Widget is disposed, bloc should be disposed.
+  @override
+  void initState() {
+    super.initState();
+    countBloc = CountBloc();
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    countBloc.dispose();
+  }
+}
+
+//Seperate updating View
+class CountView extends StatelessWidget {
+  CountView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("Countview_Bloc Pattern");
+    return Center(
+      child:StreamBuilder(
+        //Streambuilder instead of other widget
+        stream: countBloc.count,
+        initialData: 0,
+        builder: (BuildContext contex, AsyncSnapshot<int> snapshot) {
+          if(snapshot.hasData){
+            return Text(
+              snapshot.data.toString(),
+              style: TextStyle(fontSize: 88),
+            );
+          };
+
+          return CircularProgressIndicator();
+        },
+
+      ),
+    );
+  }
+
+
 
 
 }
